40918e519415ad49eada3ef4a4d0cad140098a6a
Riii28 <gtrskyline2006@gmail.com>
2025-02-21 20:04:38 +0700

    initial

--- components/Confirm.tsx
+++ components/Confirm.tsx
@@ -10,9 +10,9 @@ export default function Confirm({ title, func }: { title: string; func: string }
     const { confirmState, setConfirmState } = useAppState();
     const [handle, setHandle] = useState<() => Promise<void> | undefined>();
 
-    const handleClearNotes = useCallback(async () => {
+    const handleAction = useCallback(async (endpoint: string) => {
         try {
-            const response = await fetch("/api/notes", {
+            const response = await fetch(endpoint, {
                 method: "DELETE",
                 headers: { "Content-Type": "application/json" },
             });
@@ -28,37 +28,11 @@ export default function Confirm({ title, func }: { title: string; func: string }
                 return;
             }
 
-            setConfirmState();
             toast.success(result.message);
             refresh();
-        } catch (err) {
-            console.error("Error clearing notes:", err);
-        }
-    }, [refresh, setConfirmState]);
-
-    const handleClearFolders = useCallback(async () => {
-        try {
-            const response = await fetch("/api/folders", {
-                method: "DELETE",
-                headers: { "Content-Type": "application/json" },
-            });
-
-            if (!response.ok) {
-                toast.error("Internal server error");
-                return;
-            }
-
-            const result = await response.json();
-            if (!result.success) {
-                toast.error(result.message);
-                return;
-            }
-
-            refresh();
-            toast.success(result.message);
             setConfirmState();
         } catch (err) {
-            console.error("Error clearing folders:", err);
+            console.error(`Error clearing ${endpoint}:`, err);
         }
     }, [refresh, setConfirmState]);
 
@@ -66,30 +40,30 @@ export default function Confirm({ title, func }: { title: string; func: string }
         setHandle(() => {
             switch (func) {
                 case "CLEAR_NOTES":
-                    return handleClearNotes;
+                    return () => handleAction("/api/notes");
                 case "CLEAR_FOLDERS":
-                    return handleClearFolders;
+                    return () => handleAction("/api/folders");
                 default:
                     return undefined;
             }
         });
-    }, [func, handleClearNotes, handleClearFolders]);
+    }, [func, handleAction]);
+
+    if (!confirmState) return null;
 
     return (
-        confirmState && (
-            <div className="fixed w-full h-full z-50 bg-transparent">
-                <div className="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex flex-col gap-y-8 bg-white dark:bg-gray-800 transition-colors duration-200 w-80 p-4 rounded-md shadow-md">
-                    <h1 className="text-xl">{title}</h1>
-                    <div className="flex justify-end gap-x-6 text-dark-300 dark:text-light-100">
-                        <button onClick={setConfirmState} className="active:scale-95">
-                            Cancel
-                        </button>
-                        <button onClick={() => handle?.()} className="active:scale-95">
-                            Yes
-                        </button>
-                    </div>
+        <div className="fixed w-full h-full z-50 bg-transparent">
+            <div className="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex flex-col gap-y-8 bg-white dark:bg-gray-800 transition-colors duration-200 w-80 p-4 rounded-md shadow-md">
+                <h1 className="text-xl">{title}</h1>
+                <div className="flex justify-end gap-x-6 text-dark-300 dark:text-light-100">
+                    <button onClick={setConfirmState} className="active:scale-95">
+                        Cancel
+                    </button>
+                    <button onClick={() => handle?.()} className="active:scale-95">
+                        Yes
+                    </button>
                 </div>
             </div>
-        )
+        </div>
     );
 }
--- lib/firebase/admin.ts
+++ lib/firebase/admin.ts
@@ -1,9 +1,18 @@
 import admin from 'firebase-admin'
-import { readFileSync } from 'fs'
-import path from 'path'
 
-const serviceAccountPath = path.resolve(process.cwd(), './lib/firebase/app-notes-d9585-firebase-adminsdk-fbsvc-19a6cf9959.json')
-const serviceAccount = JSON.parse(readFileSync(serviceAccountPath, 'utf-8'))
+const serviceAccount: any  = {
+    type: process.env.TYPE,
+    project_id: process.env.PROJECT_ID,
+    private_key_id: process.env.PRIVATE_KEY_ID,
+    private_key: process.env.PRIVATE_KEY?.replace(/\\n/g, '\n'),
+    client_email: process.env.CLIENT_EMAIL,
+    client_id: process.env.CLIENT_ID,
+    auth_uri: process.env.AUTH_URI,
+    token_uri: process.env.TOKEN_URI,
+    auth_provider_x509_cert_url: process.env.AUTH_PROVIDER_CERT_URL,
+    client_x509_cert_url: process.env.CLIENT_CERT_URL,
+    universe_domain: process.env.UNIVERSE_DOMAIN  
+}
 
 if (!admin.apps.length) {
     admin.initializeApp({
